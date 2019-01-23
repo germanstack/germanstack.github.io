545e6c0023c1869462234c335ac86013bce76c1f
codeekage <agiriabrahamjunior@gmail.com>
2019-01-23 14:50:58 +0100

    redesigned homepage

---
+++ js/init.js
@@ -0,0 +1,13 @@
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       false,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
